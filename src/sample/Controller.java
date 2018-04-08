6682c9837975415f14eeddfbe736b20aea9167f9
Mateusz Szafarz <mateuszszafarz@gmail.com>
2018-04-08 14:58:25 +0200

    Ability to add new contact items has been introduced.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,15 +1,22 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.IOException;
+import java.util.Optional;
 
 public class Controller {
+
+    @FXML
+    VBox mainVBoxPane;
 
     @FXML
     TableView<Contact> table;
@@ -24,7 +31,6 @@ public class Controller {
     TableColumn<Contact, String> notes;
 
     public void initialize() {
-//        table.
 
         firstName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
         lastName.setCellValueFactory(new PropertyValueFactory<>("lastName"));
@@ -37,9 +43,40 @@ public class Controller {
     }
 
     @FXML
-    public void displayNewContactDialog() {
-        DateFormat formatter = new SimpleDateFormat("HH:mm");
-        Contact contact = Main.contactData.getContacts().get(0);
-        contact.setNotes(formatter.format(new Date()));
+    public void displayNewContactDialog() throws IOException {
+        Dialog<ButtonType> dialog = getButtonTypeDialog();
+        FXMLLoader fxmlLoader = loadFXMLView(dialog);
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+
+        if(result.isPresent() && result.get() == ButtonType.OK) {
+            NewContactDialogController controller = fxmlLoader.getController();
+            Contact newItem = controller.processResults();
+            Main.contactData.addContact(newItem);
+            table.getSelectionModel().select(newItem);
+        }
+    }
+
+    private FXMLLoader loadFXMLView(Dialog<ButtonType> dialog) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("newContactDialog.fxml"));
+        dialog.getDialogPane().setContent(fxmlLoader.load());
+        return fxmlLoader;
+    }
+
+    private Dialog<ButtonType> getButtonTypeDialog() {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainVBoxPane.getScene().getWindow());
+        dialog.setTitle("Add New Contact");
+        dialog.setHeaderText("Use this dialog to create a new contact item");
+        return dialog;
+    }
+
+    @FXML
+    public void closeTheApp() {
+        Platform.exit();
     }
 }
--- src/sample/NewContactDialogController.java
+++ src/sample/NewContactDialogController.java
@@ -1,4 +1,25 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class NewContactDialogController {
+
+    @FXML
+    TextField firstNameField;
+    @FXML
+    TextField lastNameField;
+    @FXML
+    TextField phoneNumberField;
+    @FXML
+    TextArea notesArea;
+
+    Contact processResults() {
+        String firstName = firstNameField.getText().trim();
+        String lastName = lastNameField.getText().trim();
+        String phoneNumber = phoneNumberField.getText().trim();
+        String notes = notesArea.getText().trim();
+        return new Contact(firstName, lastName, phoneNumber, notes);
+    }
 }
